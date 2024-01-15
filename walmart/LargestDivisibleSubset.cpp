3a64a6aac38071dee2c74657dcb2ebf698508785
Sanika1612 <117501602+Sanika1612@users.noreply.github.com>
2024-01-15 22:56:59 +0530

    Update LargestDivisibleSubset.cpp

--- walmart/LargestDivisibleSubset.cpp
+++ walmart/LargestDivisibleSubset.cpp
@@ -31,3 +31,41 @@ public:vector<int>ans;
        return ans;
     }
 };
+
+
+//Using tabulation (striver's video)
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,1);
+        vector<int>hash(n);
+        sort(nums.begin(), nums.end());
+        int lastIndex=0, mx=0;
+        for(int i=0; i<n; i++){
+            hash[i]=i;
+            for(int pre=0; pre<i; pre++){
+                if(nums[i]%nums[pre]==0){
+                    if(dp[i]<dp[pre]+1){
+                        hash[i]=pre;
+                        dp[i]=dp[pre]+1;
+                    }
+                    
+                }
+            }
+            if(dp[i]>mx){
+                mx=dp[i];
+                lastIndex=i;
+            }
+
+        }
+        vector<int>ans;
+        while(hash[lastIndex]!=lastIndex){
+            ans.push_back(nums[lastIndex]);
+            lastIndex=hash[lastIndex];
+        }
+        ans.push_back(nums[lastIndex]);
+        return ans;
+
+    }
+};
